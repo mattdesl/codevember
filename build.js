fc3bbdf8194bb3e93731263ecdad1f4be857aa27
mattdesl <dave.des@gmail.com>
2015-11-05 12:54:55 -0500

    updating build step

--- build.js
+++ build.js
@@ -1,3 +1,4 @@
+global.Promise = require('pinkie-promise')
 const browserify = require('browserify')
 const fs = require('fs')
 const path = require('path')
@@ -8,28 +9,39 @@ files = files.filter(function (f) {
   return /^\d+\.js$/.test(f)
 })
 
-const transforms = {
+const transformConfig = {
   '3': ['glslify']
 }
 
-files.forEach(function (f) {
-  console.log('Bundling', f)
-  var b = browserify('src/' + f, {
-    debug: false
-  })
-  b.transform(require('babelify').configure({ presets: 'es2015' }))
-  b.plugin(require('bundle-collapser/plugin'))
-  var base = path.basename(f, '.js')
-  ;(transforms[base] || []).forEach(function (t) {
-    b.transform(t)
-  })
-  b.bundle(function (err, src) {
-    if (err) throw err
-    console.log('Compressing', f)
-    var result = UglifyJS.minify(src.toString(), { fromString: true })
-    console.log('Writing', f)
-    fs.writeFile('static/' + f, result.code, function (err) {
-      if (err) throw err
+Promise.all(files.map(runBuild)).catch(function (err) {
+  console.error(err)
+}).then(function () {
+  console.log("Finished")
+})
+
+function runBuild (f) {
+  return new Promise(function (resolve, reject) {
+    console.log('Bundling', f)
+    var b = browserify('src/' + f, {
+      debug: false,
+      noparse: [ 'three' ]
+    })
+    b.transform(require('babelify').configure({ presets: 'es2015' }))
+    b.plugin(require('bundle-collapser/plugin'))
+    var base = path.basename(f, '.js')
+    var transforms = (transformConfig[base] || [])
+    transforms.forEach(function (t) {
+      b.transform(t)
+    })
+    b.bundle(function (err, src) {
+      if (err) return reject(err)
+      console.log('Compressing', f)
+      var result = UglifyJS.minify(src.toString(), { fromString: true })
+      console.log('Writing', f)
+      fs.writeFile('static/' + f, result.code, function (err) {
+        if (err) return reject(err)
+        resolve()
+      })
     })
   })
-})
+}
